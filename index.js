f09bf20c22252e8cbc9325a9c791cd64aacfc4c5
GZarateL <gael.zarate@tecsup.edu.pe>
2025-05-05 12:17:44 -0500

    Listo para ir a GitHub1

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 require('dotenv').config()
 const http = require('http')
 
-function requestController(){
-    console.log('Bienvenidos al curso')
+function requestController(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end('<h1>Servidor Pasivo</h1>')
 }
 
-const server=http.createServer(requestController)
+const server = http.createServer(requestController)
 
-const PORT=process.env.PORT
+const PORT = process.env.PORT
 
-server.listen(PORT, function(){
+server.listen(PORT, function () {
     console.log("Aplicacion corriendo en: " + PORT)
 })
+
